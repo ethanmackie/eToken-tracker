68f0b38e70da85388f7736e623fba75005422498
Maplesyrupsucker <maplesyrupsucker@protonmail.com>
2019-04-07 20:59:18 +0900

    tidy up my shit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// var json = [
-// { 'red': '#f00' },
-// { 'green': '#0f0' },
-// { 'blue': '#00f' }
-// ];
-
-// $.each(json, function () {
-//   $.each(this, function (name, value) {
-//     console.log(name + '=' + value);
-//   });
-// });
-
 String.prototype.trunc = String.prototype.trunc ||
 function(n){
     return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
@@ -17,9 +5,8 @@ function(n){
 
 
 // A comma separated list of currencies to display.
-var ticker_currencies = "USD,EUR,JPY,CNY"
 
-ticker = function(currencies) {
+tokens = function(currencies) {
 
   $.ajax({
     type: "GET",
@@ -32,11 +19,6 @@ ticker = function(currencies) {
       }
     },
     success: function (slpTokens) {
-
-
-      
-
-
       var output = [];
       var rankings = [];
 
@@ -46,11 +28,6 @@ ticker = function(currencies) {
           if (index > 100) {
               return false;
           }
-
-          // console.log(this);
-
-          // console.log(this.symbol);
-
           var symbol = this.symbol;
           var tokenId = this.tokenId;
           var name = this.name;
@@ -64,47 +41,25 @@ ticker = function(currencies) {
             symbol = "n/a";
           }
 
-
-          // console.log('Rank: ' + index + ' / txCount: ' + txCount + ' / symbol: ' + symbol + ' / tokenID: ' + tokenId + ' / name: ' + name);
           if (index < 4) { 
             output.push("<div class='podium-rank position-" + index + "'><div class='rank-" + index + "'><div class='medal'><h3 class='position'>" + index + "</h3></div><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + name + "</a></h4><h5>" + txCount + " transactions</h5><h3 class='symbol'>" + symbol + "</h3></div></div>");
           } else {
             rankings.push("<div class='rank rank-" + index + "'><h3 class='position'><span>" + index + "</span></h3><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + name + "</a></h4><h3><span class='symbol'>" + symbol + "</span></h3><h5>" + txCount + " transactions</h5></div>");
           }
-          // $.each(this, function (name, value) {
-          //   // console.log(name + '=' + value);
-          //   // console.log(this);
-          // });
+          
         });
       });
 
       $('#podium').html(output);
       $('#rankings').html(rankings)
 
-
-      // var i = 0;
-
-      // for(var i=0; i<output.length; i++) {
-
-      // }
-
-      // $.each(slpTokens, function (k, v) {
-      //   // console.log(k);
-      //   // console.log(v);
-      //   console.log(v[0]);
-      //   console.log(k);
-      //   output.push("<div class='token'>Test</div>");
-      //   i++;
-      // });
-
-      // $('#podium').html(output);
     }
   }).done(function () {
-    setTimeout(function(){ ticker(ticker_currencies); }, 20000);
+    setTimeout(function(){ tokens(); }, 10000);
   }).fail(function() {
-    setTimeout(function(){ ticker(ticker_currencies); }, 20000);
+    setTimeout(function(){ tokens(); }, 10000);
   });
 }
 
-ticker(ticker_currencies);
+tokens();
 
