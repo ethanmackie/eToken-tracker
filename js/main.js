cee995747a0389207896814d136757f492b71067
Maple <>
2019-04-12 23:51:37 +0900

    cleanup

--- js/main.js
+++ js/main.js
@@ -18,11 +18,6 @@ $(document).ready(function(){
 });
 
 tokens = function(currencies) {
-
-  // //mostAddr
-  // var mostAddr = "https://slpdb.bitcoin.com/q/ewogICJ2IjogMywKICAicSI6IHsKICAgICJkYiI6IFsidCJdLAogICAgImZpbmQiOiB7fSwKICAgICJzb3J0IjogewogICAgICAidG9rZW5TdGF0cy5xdHlfdmFsaWRfdG9rZW5fYWRkcmVzc2VzIjogLTEKICAgIH0sCiAgICAicHJvamVjdCI6IHsKICAgICAgInRva2VuRGV0YWlscy50b2tlbklkSGV4IjogMSwKICAgICAgInRva2VuU3RhdHMucXR5X3ZhbGlkX3Rva2VuX2FkZHJlc3NlcyI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMubmFtZSI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMuc3ltYm9sIjogMSwKICAgICAgInRva2VuRGV0YWlscy5kb2N1bWVudFVyaSI6IDEKICAgIH0sCiAgICAibGltaXQiOiAxMDAwCiAgfSwKICAiciI6IHsKICAgICJmIjogIlsuW10gfCB7dG9rZW5JZDogLnRva2VuRGV0YWlscy50b2tlbklkSGV4LCBhZGRyZXNzQ291bnQ6IC50b2tlblN0YXRzLnF0eV92YWxpZF90b2tlbl9hZGRyZXNzZXMsICBuYW1lOiAudG9rZW5EZXRhaWxzLm5hbWUsICBzeW1ib2w6IC50b2tlbkRldGFpbHMuc3ltYm9sLCB1cmw6IC50b2tlbkRldGFpbHMuZG9jdW1lbnRVcml9XSIKICB9Cn0=";
-
-
   $.ajax({
     type: "GET",
     url: "https://slpdb.bitcoin.com/q/ewogICJ2IjogMywKICAicSI6IHsKICAgICJkYiI6IFsidCJdLAogICAgImZpbmQiOiB7fSwKICAgICJzb3J0IjogewogICAgICAidG9rZW5TdGF0cy5xdHlfdmFsaWRfdHhuc19zaW5jZV9nZW5lc2lzIjogLTEKICAgIH0sCiAgICAicHJvamVjdCI6IHsKICAgICAgInRva2VuRGV0YWlscy50b2tlbklkSGV4IjogMSwKICAgICAgInRva2VuU3RhdHMucXR5X3ZhbGlkX3R4bnNfc2luY2VfZ2VuZXNpcyI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMubmFtZSI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMuc3ltYm9sIjogMSwKICAgICAgInRva2VuRGV0YWlscy5kb2N1bWVudFVyaSI6IDEKICAgIH0sCiAgICAibGltaXQiOiAxMDAwCiAgfSwKICAiciI6IHsKICAgICJmIjogIlsuW10gfCB7dG9rZW5JZDogLnRva2VuRGV0YWlscy50b2tlbklkSGV4LCB0cmFuc2FjdGlvbkNvdW50OiAudG9rZW5TdGF0cy5xdHlfdmFsaWRfdHhuc19zaW5jZV9nZW5lc2lzLCAgbmFtZTogLnRva2VuRGV0YWlscy5uYW1lLCAgc3ltYm9sOiAudG9rZW5EZXRhaWxzLnN5bWJvbCwgdXJsOiAudG9rZW5EZXRhaWxzLmRvY3VtZW50VXJpfV0iCiAgfQp9",
@@ -65,7 +60,7 @@ tokens = function(currencies) {
           } else {
             rankings.push("<div class='rank rank-" + index + "'><h3 class='position'><span>" + index + "</span></h3><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + name + "</a></h4><h3><span class='symbol'>" + symbol + "</span></h3><h5>" + txCount + " transactions</h5></div>");
           }
-          console.log('Poked slpDB for the goods')
+          // console.log('Poked slpDB for the goods')
           
         });
       });
@@ -81,15 +76,8 @@ tokens = function(currencies) {
   });
 }
 
-tokens();
-
-
-
-
-
 
 tokensAddr = function(currencies) {
-
   $.ajax({
     type: "GET",
     url: "https://slpdb.bitcoin.com/q/ewogICJ2IjogMywKICAicSI6IHsKICAgICJkYiI6IFsidCJdLAogICAgImZpbmQiOiB7fSwKICAgICJzb3J0IjogewogICAgICAidG9rZW5TdGF0cy5xdHlfdmFsaWRfdG9rZW5fYWRkcmVzc2VzIjogLTEKICAgIH0sCiAgICAicHJvamVjdCI6IHsKICAgICAgInRva2VuRGV0YWlscy50b2tlbklkSGV4IjogMSwKICAgICAgInRva2VuU3RhdHMucXR5X3ZhbGlkX3Rva2VuX2FkZHJlc3NlcyI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMubmFtZSI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMuc3ltYm9sIjogMSwKICAgICAgInRva2VuRGV0YWlscy5kb2N1bWVudFVyaSI6IDEKICAgIH0sCiAgICAibGltaXQiOiAxMDAwCiAgfSwKICAiciI6IHsKICAgICJmIjogIlsuW10gfCB7dG9rZW5JZDogLnRva2VuRGV0YWlscy50b2tlbklkSGV4LCBhZGRyZXNzQ291bnQ6IC50b2tlblN0YXRzLnF0eV92YWxpZF90b2tlbl9hZGRyZXNzZXMsICBuYW1lOiAudG9rZW5EZXRhaWxzLm5hbWUsICBzeW1ib2w6IC50b2tlbkRldGFpbHMuc3ltYm9sLCB1cmw6IC50b2tlbkRldGFpbHMuZG9jdW1lbnRVcml9XSIKICB9Cn0=",
@@ -132,7 +120,7 @@ tokensAddr = function(currencies) {
           } else {
             rankings.push("<div class='rank rank-" + index + "'><h3 class='position'><span>" + index + "</span></h3><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + name + "</a></h4><h3><span class='symbol'>" + symbol + "</span></h3><h5>" + txCount + " addresses</h5></div>");
           }
-          console.log('Poked slpDB for the goods')
+          // console.log('Poked slpDB for the most addr goods')
           
         });
       });
@@ -142,11 +130,12 @@ tokensAddr = function(currencies) {
 
     }
   }).done(function () {
-    setTimeout(function(){ tokensAddr(); }, 60000);
+    setTimeout(function(){ tokensAddr(); }, 65000);
   }).fail(function() {
-    setTimeout(function(){ tokensAddr(); }, 60000);
+    setTimeout(function(){ tokensAddr(); }, 65000);
   });
 }
 
+tokens();
 tokensAddr();
 
