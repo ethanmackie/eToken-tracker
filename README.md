413f420d0d71a771481099f29c874e357ec70b92
ethan <Ethanmackie1199@gmail.com>
2021-11-08 10:26:29 +1100

    Updated to parse the XEC chain for eTokens

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Podium.cash (formerly simple-token-standings)
-View SLP Simple Tokens with the most activity. Visit http://podium.cash
+# eToken Explorer
+View eTokens with the most transactional activities. Visit http://TODO
 
 # TEAM
 
 Corbin -- Dev -- @maplesyrupsuckr (on twitter)
 
  
-# SLPDB
+# TokenDB
 
 View https://github.com/simpleledger/SLPDB for more inforomation on SLPDB 
  
-#### SLPDB Top tokens by tx: 
-https://slpdb.bitcoin.com/explorer2/ewogICJ2IjogMywKICAicSI6IHsKICAgICJkYiI6IFsidCJdLAogICAgImZpbmQiOiB7fSwKICAgICJzb3J0IjogewogICAgICAidG9rZW5TdGF0cy5xdHlfdmFsaWRfdHhuc19zaW5jZV9nZW5lc2lzIjogLTEKICAgIH0sCiAgICAicHJvamVjdCI6IHsKICAgICAgInRva2VuRGV0YWlscy50b2tlbklkSGV4IjogMSwKICAgICAgInRva2VuU3RhdHMucXR5X3ZhbGlkX3R4bnNfc2luY2VfZ2VuZXNpcyI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMubmFtZSI6IDEsCiAgICAgICJ0b2tlbkRldGFpbHMuc3ltYm9sIjogMSwKICAgICAgInRva2VuRGV0YWlscy5kb2N1bWVudFVyaSI6IDEKICAgIH0sCiAgICAibGltaXQiOiAxMDAwCiAgfSwKICAiciI6IHsKICAgICJmIjogIlsuW10gfCB7dG9rZW5JZDogLnRva2VuRGV0YWlscy50b2tlbklkSGV4LCB0cmFuc2FjdGlvbkNvdW50OiAudG9rZW5TdGF0cy5xdHlfdmFsaWRfdHhuc19zaW5jZV9nZW5lc2lzLCAgbmFtZTogLnRva2VuRGV0YWlscy5uYW1lLCAgc3ltYm9sOiAudG9rZW5EZXRhaWxzLnN5bWJvbCwgdXJsOiAudG9rZW5EZXRhaWxzLmRvY3VtZW50VXJpfV0iCiAgfQp9
+
+# Attribution
+
+Original codebase forked from https://github.com/maplesyrupsucker/simple-token-standings
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 String.prototype.trunc = String.prototype.trunc ||
 function(n){
     return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
@@ -19,62 +21,68 @@ $(document).ready(function(){
 
 function imgError(image) {
     image.onerror = "";
-    image.src = "img/icon/slp.png";
+    image.src = "img/icon/ecash-icon.png";
     return true;
 }
 
 tokens = function(currencies) {
   $.ajax({
     type: "GET",
-    url: "https://slpdb.fountainhead.cash/q/ewogICJ2IjogMywKICAicSI6IHsKICAgICJkYiI6IFsidCJdLAogICAgImZpbmQiOiB7fSwKICAgICJzb3J0IjogewogICAgICAidG9rZW5TdGF0cy5hcHByb3hfdHhuc19zaW5jZV9nZW5lc2lzIjogLTEKICAgIH0sCiAgICAibGltaXQiOiAxMDAsCiAgICAic2tpcCI6IDAKICB9Cn0=",
+    url: "https://tokendb.kingbch.com/q/ew0KICAidiI6IDMsDQogICJxIjogew0KICAgICJkYiI6IFsidCJdLA0KICAgICJmaW5kIjogeyJ0b2tlblN0YXRzLmJsb2NrX2NyZWF0ZWQiOiB7IiRndCI6IDY4NTMyMn0sICJ0b2tlbkRldGFpbHMudmVyc2lvblR5cGUiOiAxfSwNCiAgICAicHJvamVjdCI6IHsidG9rZW5EZXRhaWxzIjogMSwgInRva2VuU3RhdHMiOiAxLCAiX2lkIjogMH0sDQogICAgInNvcnQiOiB7InRva2VuU3RhdHMuYXBwcm94X3R4bnNfc2luY2VfZ2VuZXNpcyI6IC0xfSwNCiAgICAibGltaXQiOiAyMA0KICB9DQp9",
     contentType: "application/json; charset=utf-8",
     timeout: 6000,
     error: function (x, t, m) {
       if ($('#tab-mostTx .podium').html() === '') {
-        $("#tab-mostTx .podium").html("<div class='currency'><h4>Error retrieving token count</h4></div>");
+        $("#tab-mostTx .podium").html("<div class='currency'><h4>Error retrieving eToken count</h4></div>");
       }
     },
     complete: function () { 
       $('#ajax-loader').hide();
     },
-    success: function (slpTokens) {
-
+    success: function (eTokens) {
       var output = [];
       var rankings = [];
 
-      $.each(slpTokens, function () {
+      $.each(eTokens, function () {
         $.each(this, function (index) {
-            console.log(this);
 
-            index = index+1;
+          index = index+1;
           if (index > 100) {
               return false;
           }
-          var symbol = this.tokenDetails.symbol;
-          var tokenId = this.tokenDetails.tokenIdHex;
-          var name = this.tokenDetails.name;
-          var txCount = this.tokenStats.approx_txns_since_genesis;
-          var url = this.tokenDetails.documentUri;
 
-          if (name == '') {
-            name = tokenId.trunc(8);
-          }
-          if (symbol == '') { 
-            symbol = "n/a";
-          }
+          var symbol = '';
+          var tokenId = '';
+          var name = '';
+          var txCount = '';
+          var url = '';
           
-          icon = "<img src='https://tokens.bch.sx/64/" + tokenId + ".png' class='icon' onerror='imgError(this);' style='opacity:1;'>";
-
-          if (index < 4) { 
-            output.push("<div class='podium-rank position-" + index + "'><div class='rank-" + index + "'><div class='medal'><h3 class='position'>" + index + "</h3></div><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + icon + name + "</a></h4><h5>" + txCount.toLocaleString('en') + " transactions</h5><h3 class='symbol'>" + symbol + "</h3></div></div>");
-          } else {
-            rankings.push("<div class='rank rank-" + index + "'><h3 class='position'><span>" + index + "</span></h3><h4><a href='https://explorer.bitcoin.com/bch/tx/" + tokenId + "'>" + icon + name + "</a></h4><h3><span class='symbol'>" + symbol + "</span></h3><h5>" + txCount.toLocaleString('en') + " transactions</h5></div>");
+          if (this.tokenDetails && this.tokenStats) {
+            symbol = this.tokenDetails.symbol;
+            tokenId = this.tokenDetails.tokenIdHex;
+            name = this.tokenDetails.name;
+            txCount = this.tokenStats.approx_txns_since_genesis;
+            url = this.tokenDetails.documentUri;
+            
+            var tokenTimestamp;
+            if (this.tokenStats.block_created && this.tokenDetails.versionType===1) {
+                if (name == '') {
+                  name = tokenId.trunc(8);
+                }
+                if (symbol == '') { 
+                  symbol = "n/a";
+                }
+                
+                icon = "<img src='img/icon/ecash-icon.png' class='icon' onerror='imgError(this);' style='opacity:1;'>";
+                if (index < 4) { 
+                  output.push("<div class='podium-rank position-" + index + "'><div class='rank-" + index + "'><div class='medal'><h3 class='position'>" + index + "</h3></div><h4><a href='https://explorer.be.cash/tx/" + tokenId + "' target='_blank'>" + icon + name + "</a></h4><h5>" + txCount.toLocaleString('en') + " transactions</h5><h3 class='symbol'>" + symbol + "</h3></div></div>");
+                } else {
+                  rankings.push("<div class='rank rank-" + index + "'><h3 class='position'><span>" + index + "</span></h3><h4><a href='https://explorer.be.cash/tx/" + tokenId + "' target='_blank'>" + icon + name + "</a></h4><h3><span class='symbol'>" + symbol + "</span></h3><h5>" + txCount.toLocaleString('en') + " transactions</h5></div>");
+                }
+            }
           }
-          // console.log('Poked slpDB for the goods')
-          
         });
       });
-
       $('#tab-mostTx .podium').html(output);
       $('#tab-mostTx .rankings').html(rankings)
 
